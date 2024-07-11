9fd7fc0ffaf063f67393ffd735f52ce2c0907b06
sujaramesh31 <106755559+sujaramesh31@users.noreply.github.com>
2024-07-11 18:05:07 -0400

    Update streamlit_app.py
    Removing  get_active_session() error

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ name_on_order = st.text_input("Name on Smoothie:")
 if name_on_order:
     st.write("The name on your smoothie will be", name_on_order)
     
-    session = get_active_session()
+   
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
     #st.dataframe(data=my_dataframe, use_container_width=True)
     
