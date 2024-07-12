d616c79b018b53522d7b9465bb502b02f4eb0b42
sujaramesh31 <106755559+sujaramesh31@users.noreply.github.com>
2024-07-11 21:47:47 -0400

    Update streamlit_app.py
    Hooking up the search on value

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ if name_on_order:
             st.subheader(fruit_chosen + ' Nutrition Information')
 
             search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+            #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
             
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         #st.write(ingredients_string)
     
