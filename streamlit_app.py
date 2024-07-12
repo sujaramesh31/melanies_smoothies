6f3a6ebb1416677d70ccb5e41eebd28c8b20b25e
sujaramesh31 <106755559+sujaramesh31@users.noreply.github.com>
2024-07-11 20:53:49 -0400

    Update streamlit_app.py
    Showing nutrition for fruits chosen.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 
 from snowflake.snowpark.functions import col
 
@@ -29,6 +30,9 @@ if name_on_order:
         ingredients_string = ''
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + 'Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen")
+            fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         #st.write(ingredients_string)
     
         #Save to table
@@ -46,9 +50,7 @@ if name_on_order:
 else:
     st.error("Please tell us your name!")
 
-#New section to display fruityvise nutrition info
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
+
 
