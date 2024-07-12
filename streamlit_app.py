96b7b628fd6d398212f460b431d29fe7d04b9d51
sujaramesh31 <106755559+sujaramesh31@users.noreply.github.com>
2024-07-11 21:40:25 -0400

    Update streamlit_app.py
    Adding the pandas dataframe and finding the search o for the selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ if name_on_order:
     #st.stop()
 
     pd_df = my_dataframe.to_pandas()
-    st.dataframe(pd_df)
-    st.stop
+    #st.dataframe(pd_df)
+    #st.stop()
     
     ingredients_list = st.multiselect("Choose upto 5 ingredients:", my_dataframe, max_selections = 5)
     
@@ -37,6 +37,10 @@ if name_on_order:
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
             st.subheader(fruit_chosen + ' Nutrition Information')
+
+            search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+            
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         #st.write(ingredients_string)
