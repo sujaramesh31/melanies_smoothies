cbe41b970a2f63964df03e073c67c65e112087f1
sujaramesh31 <106755559+sujaramesh31@users.noreply.github.com>
2024-07-11 21:34:54 -0400

    Update streamlit_app.py
    Coverting to pandas data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 
 from snowflake.snowpark.functions import col
 
@@ -19,8 +20,13 @@ if name_on_order:
     
    
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-    st.dataframe(data=my_dataframe, use_container_width=True)
-    st.stop()
+    #st.dataframe(data=my_dataframe, use_container_width=True)
+    #st.stop()
+
+    pd_df = my_dataframe.to_pandas()
+    st.dataframe(pd_df)
+    st.stop
+    
     ingredients_list = st.multiselect("Choose upto 5 ingredients:", my_dataframe, max_selections = 5)
     
     if ingredients_list:
